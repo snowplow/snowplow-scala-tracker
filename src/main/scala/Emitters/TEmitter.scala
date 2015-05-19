432c5f3492e5c632fa8ecb5f61af1448d36d43b9
Fred Blundun <fred@snowplowanalytics.com>
2015-05-19 13:53:32 +0100

    Removed unnecessary TEmitter methods

--- src/main/scala/Emitters/TEmitter.scala
+++ src/main/scala/Emitters/TEmitter.scala
@@ -78,13 +78,5 @@ object TEmitter {
 
 trait TEmitter {
 
-  def httpGet(endpoint: String, evt: Map[String, String]) {
-    
-  }
-
-  def blockingGet(event: Map[String, String], endpoint: String) {
-
-  }
-
   def input(event: Map[String, String]): Unit
 }
