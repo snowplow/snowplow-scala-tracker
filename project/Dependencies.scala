9207422e4eaeb41ab1a1dfc6e1afcd430cff8226
John Ferrolino <john@ferrl.in>
2015-06-11 04:43:32 +0800

    adding akka-stream/http dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,8 @@ object Dependencies {
     val scalaUtil   = "0.1.0"
     val json4s      = "3.2.11"
     val sprayClient = "1.3.2"
-    val akka        = "2.3.5"
+    val akka        = "2.3.11"
+    val akkaHttp    = "1.0-RC3"
     object collUtil {
       val _29       = "5.3.10"
       val _210      = "6.3.4"
@@ -65,7 +66,10 @@ object Dependencies {
 
     // Scala
     val scalaUtil   = "com.snowplowanalytics"      %  "scala-util"        % V.scalaUtil
-    val sprayClient = "io.spray"                   %% "spray-client"      % V.sprayClient
+    // val sprayClient = "io.spray"                   %% "spray-client"      % V.sprayClient
+    val akkaHttp    = "com.typesafe.akka"          %% "akka-http-experimental"      % V.akkaHttp
+    val akkaStream  = "com.typesafe.akka"          %% "akka-stream-experimental"    % V.akkaHttp
+    val akkaHCore   = "com.typesafe.akka"          %% "akka-http-core-experimental" % V.akkaHttp
     val akka        = "com.typesafe.akka"          %% "akka-actor"        % V.akka
     val json4sJackson = "org.json4s"               %% "json4s-jackson"    % V.json4s
     object collUtil {
