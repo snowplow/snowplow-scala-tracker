750168123b3351a938b212d81bbd922f591c7a50
Anton Parkhomenko <mailbox@chuwy.me>
2016-05-10 15:39:50 +0300

    Prepared for release
    Add sync to mavencentral on deployments
    
    After ~2 hours successfully deployed artifacts should appear on
    search.maven.org

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -17,6 +17,7 @@ pwd
 project_version=$(sbt version -Dsbt.log.noformat=true | perl -ne 'print $1 if /(\d+\.\d+[^\r\n]*)/')
 if [ "${project_version}" == "${tag_version}" ]; then
     sbt +publish
+    sbt +bintraySyncMavenCentral
 else
     echo "Tag version '${tag_version}' doesn't match version in scala project ('${project_version}'). Aborting!"
     exit 1
