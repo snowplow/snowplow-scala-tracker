5507f2f59aba0c1bb8b64c24b51a8a308e68a3fe
Anton Parkhomenko <mailbox@chuwy.me>
2017-10-13 17:38:18 +0700

    Bump SBT to 1.0.2 (close #53)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015 Snowplow Analytics Ltd. All rights reserved.
+ * Copyright (c) 2013-2017 Snowplow Analytics Ltd. All rights reserved.
  *
  * This program is licensed to you under the Apache License Version 2.0,
  * and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -11,77 +11,39 @@
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
 import sbt._
-import Keys._
 
 object Dependencies {
 
-  val resolutionRepos = Seq(
-    // For Twitter's LRU cache
-    "Twitter Maven Repo" at "http://maven.twttr.com/",
-    "Sonatype" at "https://oss.sonatype.org/content/repositories/releases"
-  )
-
   object V {
     // Java
-    val commonsLang = "3.1"
     val commonsCodec = "1.5"
-    val jodaTime    = "2.3"
-    val jodaMoney   = "0.9"
-    val jodaConvert = "1.2"
-    val jackson     = "1.9.7"
-    
+
     // Scala
     val json4s      = "3.2.11"
     val spray       = "1.3.3"
     val akka        = "2.3.14"
-    object collUtil {
-      val _210      = "6.3.4"
-      val _211      = "6.23.0"
-    }
 
     // Java (test only)
     val mockito     = "1.9.5"
 
     // Scala (test only)
-    object specs2 {
-      val _210      = "2.3.13"
-      val _211      = "2.3.13"
-    }
+    val specs2      = "2.3.13"
   }
 
   object Libraries {
     // Java
-    val commonsCodec = "commons-codec"             % "commons-codec"      % V.commonsCodec
-    val commonsLang = "org.apache.commons"         % "commons-lang3"      % V.commonsLang
-    val jodaTime    = "joda-time"                  % "joda-time"          % V.jodaTime
-    val jodaMoney   = "org.joda"                   % "joda-money"         % V.jodaMoney
-    val jodaConvert = "org.joda"                   % "joda-convert"       % V.jodaConvert
-    val jackson     = "org.codehaus.jackson"       % "jackson-mapper-asl" % V.jackson
+    val commonsCodec  = "commons-codec"     % "commons-codec"     % V.commonsCodec
 
     // Scala
-    val sprayClient = "io.spray"                   %% "spray-client"      % V.spray
-    val akka        = "com.typesafe.akka"          %% "akka-actor"        % V.akka
-    val json4sJackson = "org.json4s"               %% "json4s-jackson"    % V.json4s
-    object collUtil {
-      val _210      = "com.twitter"                %% "util-collection"   % V.collUtil._210
-      val _211      = "com.twitter"                %% "util-collection"   % V.collUtil._211
-    }
+    val sprayClient   = "io.spray"          %% "spray-client"      % V.spray
+    val akka          = "com.typesafe.akka" %% "akka-actor"        % V.akka
+    val json4sJackson = "org.json4s"        %% "json4s-jackson"    % V.json4s
 
     // Java (test only)
-    val mockito     = "org.mockito"                %  "mockito-all"       % V.mockito            % "test"
+    val mockito       = "org.mockito"       %  "mockito-all"       % V.mockito            % "test"
 
     // Scala (test only)
-    object specs2 {
-      val _210      = "org.specs2"                 %% "specs2"            % V.specs2._210        % "test"
-      val _211      = "org.specs2"                 %% "specs2"            % V.specs2._211        % "test"
-    }
-    val sprayTest   = "io.spray"                   %% "spray-testkit"     % V.spray              % "test"
+    val specs2        = "org.specs2"        %% "specs2"            % V.specs2             % "test"
+    val sprayTest     = "io.spray"          %% "spray-testkit"     % V.spray              % "test"
   }
-
-  def onVersion[A](all: Seq[A] = Seq(), on210: => Seq[A] = Seq(), on211: => Seq[A] = Seq()) =
-    scalaVersion(v => all ++ (if (v.contains("2.10.")) {
-      on210
-    } else {
-      on211
-    }))
 }
