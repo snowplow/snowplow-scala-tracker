c9732a44496a4221326e63db9f8e753601aad9c6
Jan Tatarik <jtatarik@liveintent.com>
2018-01-25 12:25:59 +0100

    Track page URL in the tracker subject

--- src/test/scala/com.snowplowanalytics.snowplow.scalatracker/TrackerSpec.scala
+++ src/test/scala/com.snowplowanalytics.snowplow.scalatracker/TrackerSpec.scala
@@ -86,6 +86,7 @@ class TrackerSpec extends Specification {
         .setIpAddress("255.255.255.255")
         .setUseragent("Mozilla/5.0 (Windows NT 5.1; rv:24.0) Gecko/20100101 Firefox/24.0")
         .setNetworkUserId("id")
+        .setPageUrl("some-url")
 
       tracker.setSubject(subject)
 
@@ -104,6 +105,7 @@ class TrackerSpec extends Specification {
       event("ip") must_== "255.255.255.255"
       event("ua") must_== "Mozilla/5.0 (Windows NT 5.1; rv:24.0) Gecko/20100101 Firefox/24.0"
       event("tnuid") must_== "id"
+      event("url") must_== "some-url"
     }
   }
 
